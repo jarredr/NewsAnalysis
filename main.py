39e35eaa9c7c5cc7621d78ce864f5123e5087761
Jarred Reilly <mail@jarredreilly.com>
2021-05-23 18:09:36 +1000

    Add sentiment analysis for popular websites

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
-from news_headlines import abc_headlines
+from collections import Counter
+from news_headlines import abc_headlines, news_dotcomau_headlines, smh_headlines, nine_headlines, australian_headlines, guardian_headlines, sbs_headlines, skynews_headlines
 from sentiment_analysis import sentiment_analysis
 
+
+def count_sentiment(sentiment):
+    """
+    Returns the frequency of each sentiment.
+        Parameters:
+            sentiment (list): the sentiment values
+        Returns:
+            a dictionary containing the frequency of each sentiment value
+
+    """
+    counts = {'pos': 0, 'neu': 0, 'neg': 0}
+    for item in sentiment:
+        counts[item] += 1
+    print(counts)
+
 if __name__ == "__main__":
-    print(sentiment_analysis(abc_headlines()))
+    count_sentiment(sentiment_analysis(abc_headlines()))
+    count_sentiment(sentiment_analysis(news_dotcomau_headlines()))
+    count_sentiment(sentiment_analysis(smh_headlines()))
+    count_sentiment(sentiment_analysis(nine_headlines()))
+    count_sentiment(sentiment_analysis(australian_headlines()))
+    count_sentiment(sentiment_analysis(guardian_headlines()))
+    count_sentiment(sentiment_analysis(sbs_headlines()))
+    count_sentiment(sentiment_analysis(skynews_headlines()))
+
--- news_headlines.py
+++ news_headlines.py
@@ -1,6 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
 
+
+def get_page(url):
+    """
+    Helper function which gets the HTML content of a website.
+        Returns:
+            BeautifulSoup object representing the web page
+    """
+    page = requests.get(url)
+    return BeautifulSoup(page.content, 'html.parser')
+
+
 def abc_headlines():
     """
     Gets the current headlines of the ABC News website.
@@ -8,9 +19,92 @@ def abc_headlines():
             list of strings representing the headlines
     """
 
-    page = requests.get("https://abc.net.au/news")
-    soup = BeautifulSoup(page.content, 'html.parser')  
+    soup = get_page("https://abc.net.au/news")
     top_stories = soup.select_one('div[data-component="TopStories"]')
     headlines_tags = top_stories.find_all('span', attrs={"data-component": "KeyboardFocus"})
     headlines = list(map(lambda h: h.text, headlines_tags))
+    return headlines
+
+
+def news_dotcomau_headlines():
+    """
+    Gets the current headlines of the news.com.au website.
+        Returns:
+            list of strings representing the headlines
+    """
+    soup = get_page("https://www.news.com.au/national/national-news")
+    headlines_tags = soup.select('h4[class="heading"] a')[:30]
+    headlines = list(map(lambda h: h.text[4:-3], headlines_tags))
+    return headlines
+
+
+def smh_headlines():
+    """
+    Gets the current headlines of the smh.com.au website.
+        Returns:
+            list of strings representing the headlines
+    """
+    soup = get_page("https://www.smh.com.au/national")
+    headlines_tags = soup.select('h3[data-testid="article-headline"] a')
+    headlines = list(map(lambda h: h.text, headlines_tags))
+    return headlines
+
+
+def nine_headlines():
+    """
+    Gets the current headlines of the 9news.com.au website.
+        Returns:
+            list of strings representing the headlines
+    """
+    soup = get_page("https://www.9news.com.au/national")
+    headlines_tags = soup.select('span[class="story__headline__text"]')[:30]
+    headlines = list(map(lambda h: h.text, headlines_tags))
+    return headlines
+
+
+def australian_headlines():
+    """
+    Gets the current headlines of the theaustralian.com.au website.
+        Returns:
+            list of strings representing the headlines
+    """
+    soup = get_page("https://www.theaustralian.com.au/nation")
+    headlines_tags = soup.select('h3[class="story-block__heading"] a')[:30]
+    headlines = list(map(lambda h: h.text, headlines_tags))
+    return headlines
+
+
+def guardian_headlines():
+    """
+    Gets the current headlines of the theguardian.com website.
+        Returns:
+            list of strings representing the headlines
+    """
+    soup = get_page("https://www.theguardian.com/australia-news/all")
+    headlines_tags = soup.select('span[class="js-headline-text"]')[:30]
+    headlines = list(map(lambda h: h.text, headlines_tags))
+    return headlines
+
+
+def sbs_headlines():
+    """
+    Gets the current headlines of the sbs.com.au website.
+        Returns:
+            list of strings representing the headlines
+    """
+    soup = get_page("https://www.sbs.com.au/news/topic/australia")
+    headlines_tags = soup.select('p[class="headline preview__headline"] a')[:30]
+    headlines = list(map(lambda h: h.text, headlines_tags))
+    return headlines
+    
+
+def skynews_headlines():
+    """
+    Gets the current headlines of the skynews.com.au website.
+        Returns:
+            list of strings representing the headlines
+    """
+    soup = get_page("https://www.skynews.com.au/page/national-news")
+    headlines_tags = soup.select('a[class="card-img-overlay"]')[:30]
+    headlines = list(map(lambda h: h.attrs['aria-label'], headlines_tags))
     return headlines
